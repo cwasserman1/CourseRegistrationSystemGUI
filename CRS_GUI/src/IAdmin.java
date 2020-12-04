3fb2cc7197e4978c9280ee8c273a34c948cdabcd
cwasserman1 <cmw9736@nyu.edu>
2020-12-03 23:32:25 -0500

    Added Student and Admin Interfaces
    Added Student and Admin Interfaces

---
+++ CRS_GUI/src/IAdmin.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public interface IAdmin {
+	public void createCourse();
+	public void deleteCourse();
+	public void editCourse();
+	public void registerStudent();
+	public void viewAllFull();
+	public void writeFull();
+	public void sortCourses();
+	public void displayCourseInfo();
+	
+}
---
+++ CRS_GUI/src/IStudent.java
@@ -0,0 +1,7 @@
+
+public interface IStudent {
+	public void viewOpenCourses();
+	public void enrollInCourse(String courseID, String sectionNumber);
+	public void dropCourse(String courseId, String sectionNumber);
+	public void viewCurrentReg();
+	}
