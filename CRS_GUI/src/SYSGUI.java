9b776270dbd3b71d3d94621893867ffcc5e98f8c
cwasserman1 <cmw9736@nyu.edu>
2020-12-06 14:28:26 -0500

    Added functionality to GUI
    Made Login buttons and formatted text

--- CRS_GUI/src/SYSGUI.java
+++ CRS_GUI/src/SYSGUI.java
@@ -1,30 +1,85 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.*;
 import java.util.*;
 public class SYSGUI {
 	JLabel titleText;
-	JFrame j;
+	JFrame frame;
+	JButton adminB, studentB,Alogin;
+	JTextField adminUser, adminPassword;
 	JPanel panel;
+	Font font1;
+	
 	public SYSGUI() {
 		initializeLayout();
+		loginUI();
+		
 	}
 	public void initializeLayout()
 	{
-		j = new JFrame();
-		j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		font1 = new Font("SansSerif", Font.BOLD, 20);
+		frame = new JFrame();
 		panel = new JPanel();
-		panel.setBounds(0, 0, 1920, 1080);
+		panel.setLayout(null);
+		panel.setBounds(0,0,1920,1080);
 		panel.setBackground(Color.cyan);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		titleText = new JLabel();
 		titleText.setText("Course Registration System");
-		//titleText.setBounds(50, 2000, 20, 0);
+		titleText.setBounds(600, 50, 300,30);
+		titleText.setFont(font1);
 		panel.add(titleText);
-		panel.setVisible(true);
-		j.add(panel);
-		j.setSize(1920,1080);
-		j.setLayout(null);
-		j.setVisible(true);
-	}	
+		//panel.setVisible(true);
+		frame.add(panel);
+		frame.setSize(1920,1080);
+		frame.setLayout(null);
+		frame.setVisible(true);
+		
+	}
+	public void loginUI() {
+		adminB = new JButton("Student Login");
+		studentB = new JButton("Admin Login");
+		adminB.setFont(font1);
+		studentB.setFont(font1);
+		panel.add(adminB);
+		panel.add(studentB);
+		adminB.setBounds(650, 500, 200, 100);
+		studentB.setBounds(650, 300, 200, 100);;
+		
+		
+	}
+	public void studentLoginUI() {
+		
+	}
+	public void studentUI() {
+		
+	}
+	public void adminUI() {
+		Alogin.setVisible(false);
+	}
+	public void adminLoginUI() {
+		Alogin = new JButton("Login");
+		Alogin.addActionListener(new ActionListener() {
+			 public void actionPerformed(ActionEvent e) {
+				 String ADMIN_USER = "Admin";
+				 String ADMIN_PASS = "Admin001";
+				 if(adminUser.getText().strip().equals(ADMIN_USER)&&adminPassword.getText().strip().equals(ADMIN_PASS)) {
+				 adminUI();
+			 }}
+		 });
+		
+	}
+	public void buttons() {
+		 adminB.addActionListener(new ActionListener() {
+			 public void actionPerformed(ActionEvent e) {
+				 studentB.setVisible(false);
+				 adminLoginUI();				 
+			 }
+		 });
+		 
+	}
 }
+
 
